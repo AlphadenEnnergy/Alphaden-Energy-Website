ce41aa8615d9e71e023957f5c26a0f476e290e1d
Donice <technocrater.pdevelopers@gmail.com>
2023-10-16 16:23:42 +0100

    CSR

---
+++ src/pages/csr/index.tsx
@@ -0,0 +1,20 @@
+import Data from "@/components/affiliates/Data";
+import Header from "@/components/affiliates/Header";
+import SubLayout from "@/layouts/SubLayout";
+import HeaderText from "@/components/shared/headerText/HeaderText";
+
+const CSR = () => {
+  return (
+    <SubLayout>
+      <Header />
+      <section className="w-full flex  flex-col justify-start items-center">
+        <div className="w-full max-w-3xl items-center justify-center mt-10">
+          <HeaderText primary={"Our"} secondary={"CSR"} />
+        </div>
+         <Data />
+      </section>
+    </SubLayout>
+  );
+};
+
+export default CSR;
