4c201c2efc2000a86870826dec698f1cab13b394
Donice <technocrater.pdevelopers@gmail.com>
2023-10-16 22:15:45 +0100

    Fix: Our Management

--- src/components/aboutUs/OurManagement.tsx
+++ src/components/aboutUs/OurManagement.tsx
@@ -21,12 +21,12 @@ const OurManagement = () => {
       <div className="w-full max-w-5xl my-14 gap-4 justify-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:px-5 lg:px-0">
         {management.map((person, i) => (
           <div key={i} className="w-auto h-80 mx-5 border-2 flex flex-col justify-end bg-gray-400 md:mx-0">
-            <div className="flex flex-col space-y-2 border p-4 m-4 text-white">
+            <div className="flex flex-col space-y-2 border p-4 m-4 text-white bg-gray-500">
               <p className="font-semibold text-lg">{person.name}</p>
               <span className="text-[10px]">{person.position}</span>
-              <Link href={person.url}>
+              {/* <Link href={person.url}>
                 <BsLinkedin className="text-xl" />
-              </Link>
+              </Link> */}
             </div>
           </div>
         ))}
