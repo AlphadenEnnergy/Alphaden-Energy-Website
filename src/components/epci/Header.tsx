ff5b5667e27d6125c9bae615077dca204de7be90
Donice <technocrater.pdevelopers@gmail.com>
2023-10-17 09:51:30 +0100

    Completed EPCI

--- src/components/epci/Header.tsx
+++ src/components/epci/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <section className="flex flex-col items-center justify-center ">
       <div className="w-full max-w-5xl mb-5 h-auto flex flex-col justify-center items-center text-center md:px-0 md:h-auto">
-        <div className="bg csr h-72 flex flex-col items-center justify-center">
+        <div className="bg epci h-72 flex flex-col items-center justify-center">
 
         </div>
       </div>
