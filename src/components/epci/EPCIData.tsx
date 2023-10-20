fef6c42f74c35e74fc7b780a5742deaf2325710b
Donice <technocrater.pdevelopers@gmail.com>
2023-10-20 15:28:22 +0100

    EPCI pictures

--- src/components/epci/EPCIData.tsx
+++ src/components/epci/EPCIData.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import image1 from "./../../assets/images/epci/1.png"
+import image2 from "./../../assets/images/epci/1a.png"
+import image3 from "./../../assets/images/epci/1b.png"
+import image4 from "./../../assets/images/epci/2.png"
+import image5 from "./../../assets/images/epci/3.png"
+import image6 from "./../../assets/images/epci/4.png"
+import image7 from "./../../assets/images/epci/5.png"
+import image8 from "./../../assets/images/epci/6.png"
+import image9 from "./../../assets/images/epci/7.png"
+import image10 from "./../../assets/images/epci/8.png"
 
 const EPCIData = () => {
   return (
@@ -13,35 +23,46 @@ const EPCIData = () => {
       various projects. Notable among these projects are Total&apos;s Egina and
       Chevron&apos;s Makaraba, Meji, and Okan projects, among others.
       <br />
-      <div className="w-full h-96 bg-gray-300 mt-4"></div>
+      <div className="w-full mt-4">
+        <Image src={image1} alt={"Project Photo"} />
+      </div>
       <br />
-      Additionally, Alphaden proudly serves as the EPC Contractor for Shell&apos;s
-      Assa North Ohaji (ANOH) South Gas Development project, awarded In 2018,
-      the contract is for the EPC, Site preparation & Industrial Area
-      Infrastructure Development by the Shell Petroleum Development Company
-      (SPDC) for their Assa North Ohaji South Gas Development Project, which
-      includes the Well Bay Area Construction, Project Fabrication, Internal &
-      External roads, Perimeter Fencing, Residential & Industrial Infrastructure
-      as well as other developments that make up the Assa North/Ohaji South
-      Field Logistics Base and Primary Treatment Facility.
+      Additionally, Alphaden proudly serves as the EPC Contractor for
+      Shell&apos;s Assa North Ohaji (ANOH) South Gas Development project,
+      awarded In 2018, the contract is for the EPC, Site preparation &
+      Industrial Area Infrastructure Development by the Shell Petroleum
+      Development Company (SPDC) for their Assa North Ohaji South Gas
+      Development Project, which includes the Well Bay Area Construction,
+      Project Fabrication, Internal & External roads, Perimeter Fencing,
+      Residential & Industrial Infrastructure as well as other developments that
+      make up the Assa North/Ohaji South Field Logistics Base and Primary
+      Treatment Facility.
       <br />
       <div className="w-full md:flex mt-4 md:space-x-4">
-        <div className="w-full md:w-1/2 h-96 bg-gray-300"></div>
-        <div className="w-full md:w-1/2 grid grid-cols-1 h-96 space-y-4">
-          <div className="w-full bg-gray-300"></div>
-          <div className="w-full bg-gray-300"></div>
+        <div className="w-full md:w-1/2 ">
+          <Image src={image2} alt={"Project Photo"} height={1000}/>
+        </div>
+        <div className="w-full md:w-1/2 grid grid-cols-1  space-y-4">
+          <div className="w-full">
+            <Image src={image3} alt={"Project Photo"} height={1000}/>
+          </div>
+          <div className="w-full">
+            <Image src={image4} alt={"Project Photo"} height={1000}/>
+          </div>
         </div>
       </div>
       <br />
-      In this capacity, the company has recently overseen the design,
-      fabrication, and recent installation of the Well Test Flare piping and
-      bundwall for the Assa North Ohaji South Gas Development Project,
-      culminating in the achievement of the coveted Golden Weld milestone for
-      the ANOH project.
+      <p className="mt-8">
+        In this capacity, the company has recently overseen the design,
+        fabrication, and recent installation of the Well Test Flare piping and
+        bundwall for the Assa North Ohaji South Gas Development Project,
+        culminating in the achievement of the coveted Golden Weld milestone for
+        the ANOH project.
+      </p>
       <br />
       <div className="w-full md:flex mt-4 md:space-x-4">
-        <div className="w-full md:w-1/2 h-96 bg-gray-300"></div>
-        <div className="w-full md:w-1/2 h-96 bg-gray-300"></div>
+        <div className="w-full md:w-1/2 "><Image src={image5} alt={"Project Photo"} height={1000}/></div>
+        <div className="w-full md:w-1/2 "><Image src={image6} alt={"Project Photo"} height={1000}/></div>
       </div>
       <br />
       ALPHADEN offers excellent turnkey services for a wide range of industrial
@@ -55,7 +76,7 @@ const EPCIData = () => {
       procurement, and construction management (EPCm) services as part of our
       extensive portfolio.
       <br />
-      <div className="w-full h-96 bg-gray-300 mt-4"></div>
+      <div className="w-full  mt-4"><Image src={image7} alt={"Project Photo"} height={1000}/></div>
       <br />
       With a wealth of OEM relationships and well-managed industry connections,
       we remain committed to our mission of delivering fit-for-purpose services
@@ -77,7 +98,7 @@ const EPCIData = () => {
       Instrumentation & Automation, Umbilicals and submarine cables, and Pumps
       and Compressors.
       <br />
-      <div className="w-full h-96 bg-gray-300 mt-4"></div>
+      <div className="w-full  mt-4"><Image src={image8} alt={"Project Photo"} height={1000}/></div>
       <br />
       Our expertise also extends to equipment, modular installation, and
       commissioning projects. All our personnel possess the requisite skills for
@@ -88,8 +109,8 @@ const EPCIData = () => {
       processes, as required by our clients.
       <br />
       <div className="w-full md:flex mt-4 md:space-x-4">
-        <div className="w-full md:w-1/2 h-96 bg-gray-300"></div>
-        <div className="w-full md:w-1/2 h-96 bg-gray-300"></div>
+        <div className="w-full md:w-1/2 "><Image src={image9} alt={"Project Photo"} height={1000}/></div>
+        <div className="w-full md:w-1/2 "><Image src={image10} alt={"Project Photo"} height={1000}/></div>
       </div>
       <br />
       Alphaden employs highly skilled structural and facilities engineering
